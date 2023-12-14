d590e5518e16b127170843b04bc6de2fcce9d2ce
YuliiaTsova <yuliiatsova@gmail.com>
2023-12-13 22:49:49 -0400

    post request order

--- src/main/java/org/comit/spring/controller/CheckoutController.java
+++ src/main/java/org/comit/spring/controller/CheckoutController.java
@@ -1,27 +1,30 @@
 package org.comit.spring.controller;
 
-import java.util.Arrays;
-
 import org.comit.spring.dto.PostCheckoutDTO;
 import org.comit.spring.entity.Checkout;
 import org.comit.spring.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/checkout")
+
 public class CheckoutController {
 
 	 @Autowired
 	    private CheckoutService checkoutService;
 
+	 @ResponseStatus(HttpStatus.OK)
 	    @PostMapping("/add")
 	    public Checkout placeCheckout(@RequestBody PostCheckoutDTO postCheckoutDTO) {
+	    	
 	 //      try {
 	            Checkout savedCheckout = checkoutService.saveCheckoutWithDetails(postCheckoutDTO);
 	         //   return Checkout.ok("Order with ID " + savedCheckout.getId() + " and details saved successfully.");
