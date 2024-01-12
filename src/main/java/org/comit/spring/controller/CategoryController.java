36cf2d32fab9e8676d11eaa6c8f6ff4780c4471b
YuliiaTsova <yuliiatsova@gmail.com>
2024-01-12 13:08:52 -0400

    add cors annotation

--- src/main/java/org/comit/spring/controller/CategoryController.java
+++ src/main/java/org/comit/spring/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.comit.spring.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 	
 	@Autowired
